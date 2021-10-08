48e85c3a6da223759a2fc7de901e168ec0c0d8ec
CountNick <meijer.nick1@gmail.com>
2021-10-08 11:05:23 +0200

    fix: data fixes

---
+++ src/components/Information.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div>
+        <h3>{{place_data.place_name}}</h3>
+        <h3>{{place_data.index}}</h3>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    
+    name: "Information",
+    props: ["place_data"]
+};
+</script>
+
+<style lang="less">
+
+
+</style>
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -13,26 +13,25 @@
 
 <script>
 
-import mapboxgl from "mapbox-gl"
+import mapboxgl from 'mapbox-gl'
 
 
 export default {
     
-    name: "Map",
+    name: 'Map',
     components: {
 
     },
 
   data() {
     return {
-      location: "",
-      access_token: "pk.eyJ1IjoiY291bnRuaWNrIiwiYSI6ImNrbHV6dTVpZDJibXgyd3FtenRtcThwYjYifQ.W_GWvRe3kX14Ef4oT50bSw",
+      access_token: 'pk.eyJ1IjoiY291bnRuaWNrIiwiYSI6ImNrbHV6dTVpZDJibXgyd3FtenRtcThwYjYifQ.W_GWvRe3kX14Ef4oT50bSw',
       center: [-11.842459871932249, 8.300982115201037],
       map: {},
       isModalVisible: false,
       loading: false,
       place_data: {
-          place_name: "plaatsie",
+          place_name: "place name",
           index: 100
       },
     }
@@ -48,39 +47,41 @@ export default {
           try {
               mapboxgl.accessToken = this.access_token;
               this.map = new mapboxgl.Map({
-                  container: "map",
-                  style: "mapbox://styles/mapbox/dark-v10",
+                  container: 'map',
+                  style: 'mapbox://styles/mapbox/dark-v10',
                   center: this.center,
                   zoom: 6
               });
           } catch(err) {
-              console.log("an error occured: ", err);
+              console.log('an error occured: ', err);
           }
       },
       async plotVillages() {
 
             this.map.on('load', () => {
 
-                const json = require("../assets/data.json")
+                const json = require('../assets/data.json')
 
                 this.map.addSource('villages', {
-                    type: "geojson",
+                    type: 'geojson',
                     data: json
                 });
             
                 this.map.addLayer({
-                    id: "villages-layer",
-                    type: "circle",
-                    source: "villages",
-                    // paint: {
-                    //     "fill-color": "rgba(200, 100, 240, 0.4)",
-                    //     "fill-outline-color" : "rgba(200, 100, 240, 1)"
-                    // }
+                    id: 'villages-layer',
+                    type: 'circle',
+                    source: 'villages',
+                    paint: {
+                        'circle-color': 'rgba(234, 87, 5, 0.6)',
+                        'circle-stroke-color' : 'rgba(253, 154, 240, 1)',
+                        'circle-stroke-width': 1,
+
+                    }
                 });
                 // console.log("place data: ", this.place_data)
                 
 
-                this.map.on("click", "villages-layer", (e) => {
+                this.map.on('click', 'villages-layer', (e) => {
                     
                     // store features in variable
                     const clickedPlaceData = e.features[0].properties
