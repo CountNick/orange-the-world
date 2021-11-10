adf0a4bc7c67f234eed06f80ec7c34d79353abf9
CountNick <meijer.nick1@gmail.com>
2021-11-10 10:15:45 +0100

    content: add texts data.json introduction.vue

--- src/components/Introduction.vue
+++ src/components/Introduction.vue
@@ -1,16 +1,16 @@
 <template>
     <transition name="page-fade">
     <article class="introduction__article" v-show="loading == false">
-        <h1 class="introduction__title">The walk against gender based violence</h1>
+        <h1 class="introduction__title">Welcome to the Orange Challenge!</h1>
         <div class="introduction__summary">
             <img class="summary__img" src="https://static.spacecrafted.com/f37fddf4d72c4bd1b48e97008a4a78ba/i/d29ef1e47a324d899cec873587a04846/1/4SoifmQp45JMgBnHp7ed2/Hindolo%20Pokawa.jpg" alt="">
 
             <p class="summary__text">
-            This is Hindolo Pokawa. He realised violence against women and girls is a big problem in Sierra Leone and wanted to know what the root of this problem is and try to raise awareness for this problem. He then knew what his mission would be, go to Sierra Leone and walk through different towns and villages to spread awareness for this world problem.
+            Thank you for participating! We greatly appreciate your efforts in combating violence against women in Sierra Leone with Hindolo Pokawa and UN Women NL. Hindolo's journey will take him through different cities and villages, crossing a total of 837 kilometers across Sierra Leone. This website will take you to the 10 biggest cities and villages that Hindolo visits on his walk. We will show you the activities that are done while he is there. You will be joining him in this journey, walking a total of 8.37 kilometers in your own neighborhood. We encourage you to share pictures of your walk for your sponsors! Make sure to use the hashtag #OrangetheWorldNL. 
             </p>
         </div>
       
-        <router-link class="introduction__btn cta-btn" to="/explanation" tag="button">Start your challenge</router-link>
+        <router-link class="introduction__btn cta-btn" to="/explanation" tag="button">Let's start</router-link>
     
     </article>
     </transition>
